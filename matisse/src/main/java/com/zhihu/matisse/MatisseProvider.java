bb2b1c5acfbc2fe3856fe711f454d37e4934bfc4
colin <wanglin2046@126.com>
2017-07-05 14:04:56 +0800

    # 修正FileProvider重名导致编译不过的bug

---
+++ matisse/src/main/java/com/zhihu/matisse/MatisseProvider.java
@@ -0,0 +1,19 @@
+/*
+ * *********************************************************
+ *   author   colin
+ *   company  fosung
+ *   email    wanglin2046@126.com
+ *   date     17-7-5 下午2:03
+ * ********************************************************
+ */
+package com.zhihu.matisse;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Entry for Matisse's media selection.
+ */
+public final class MatisseProvider extends FileProvider {
+
+  
+}
